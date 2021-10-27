42f4f21b7d4ac687efbf93c873bcec32e93d69c1
Quang <htquangg@gmail.com>
2021-10-27 17:53:38 +0700

    feat(roles): add permissions for each role

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,12 +1,21 @@
+const MODES = {
+  CREATE: 'create',
+  GET: 'get',
+  MANAGE: 'manage',
+  GET_USERS: 'getUsers',
+  MANAGE_USERS: 'manageUsers',
+};
+
 const allRoles = {
-  user: [],
-  admin: ['getUsers', 'manageUsers'],
+  user: [MODES.CREATE, MODES.GET, MODES.MANAGE],
+  admin: [MODES.GET_USERS, MODES.MANAGE_USERS, MODES.CREATE, MODES.GET, MODES.MANAGE],
 };
 
 const roles = Object.keys(allRoles);
 const roleRights = new Map(Object.entries(allRoles));
 
 module.exports = {
+  MODES,
   roles,
   roleRights,
 };
